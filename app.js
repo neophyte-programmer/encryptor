e3934efa0b55116e8789bcb031249892010e5208
Nutifafa Attor <attorfafa@gmail.com>
2022-06-04 08:41:34 +0000

    encryption code

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const alphabetStart = 'abcdefghijklm'
+const alphabetEnd = 'nopqrstuvwxyz'
 
+const encryptText = () => {
+	var encryptedText = ''
+	var text = encryptor.textToEncrypt.value.toLowerCase()
 
-// Main
-const main = () => {
-    // All functions will be called here.
+	// Iterate through each character in the text to check if it is in alphabetStart or alphabetEnd
+	for (var i = 0; i < text.length; i++) {
+		var letterToCheck = text[i]
+		if (alphabetStart.indexOf(letterToCheck) >= 0) {
+			encryptedText += alphabetEnd[alphabetStart.indexOf(letterToCheck)]
+		} else if (alphabetEnd.indexOf(letterToCheck) >= 0) {
+			encryptedText += alphabetStart[alphabetEnd.indexOf(letterToCheck)]
+		} else {
+			encryptedText += letterToCheck
+		}
+	}
+
+	document.getElementById('encryptedText').value = encryptedText
 }
-
-// Call Main
-main()
