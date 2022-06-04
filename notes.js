5a2e58faaceaf3a3dbc1eadc3df1e1d2008d38be
Nutifafa Attor <attorfafa@gmail.com>
2022-06-04 08:44:27 +0000

    update

--- notes.js
+++ notes.js
@@ -1,6 +1,20 @@
 /*
 THIS FILE IS A REFERENCE FILE FOR THE APP.JS FILE AND CONTAINS THE ALGORITHMS AND THOUGHT PROCESSES FOR THE APP.
 
+My Algorithm for Cesar Cipher:
+
+Encryption:
+1. Store first half of the alphabet in string variable
+2. Store second half of the alphabet in string variable
+3. Extract the message from the input field
+4. Convert the message to lowercase
+5. Loop through each character in the message
+6. If the character is in the first half of the alphabet, replace it with the corresponding letter in the second half of the alphabet
+7. If the character is in the second half of the alphabet, replace it with the corresponding letter in the first half of the alphabet
+8. If the character is not in the alphabet, leave it as is
+9. Display the encrypted message in the output field
+
+This function works both ways.
 
 
 */
